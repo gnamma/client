1876954e345a1707bdd4e15bc151252b0781b795
Harrison Shoebridge <harrison@theshoebridges.com>
2016-09-14 18:41:38 +1000

    Add basic world.xml file loading and building
    Currently loads... wait for it... BOXES!

---
+++ Assets/Scripts/RoomBuilder.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System;
+
+public class RoomBuilder : MonoBehaviour {
+    private XmlDocument doc;
+
+	// Use this for initialization
+	void Start () {
+        TextAsset text = Resources.Load("world") as TextAsset;
+        if (!text) {
+            Debug.Log("Couldn't find world[.xml]");
+            return;
+        }
+
+        doc = new XmlDocument();
+        doc.LoadXml(text.ToString());
+
+        ProcessRoom(doc.SelectNodes("room/el"));
+	}
+
+    private void ProcessRoom(XmlNodeList nodes) {
+        foreach (XmlNode node in nodes) {
+            if (node.Name != "el") {
+                throw new Exception("Unknown GWML element" + node.Name);
+            }
+
+            if (node.Attributes["model"].Value == "box") {
+                GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                go.name = node.Attributes["name"].Value;
+            }
+        }
+    }
+}
